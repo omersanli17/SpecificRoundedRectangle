7ac081d9bb0cd6f28e96f0bf7eadf52a631ecd7c
Ömer Şanlı <drasonli17@gmail.com>
2022-01-14 00:27:51 +0300

    Create RoundedRectangleShape.swift

---
+++ RoundedRectangleShape.swift
@@ -0,0 +1,25 @@
+//
+//  RoundedRectangleShape.swift
+//  MadenApp
+//
+//  Created by omer sanli on 11.01.2022.
+//
+
+import SwiftUI
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
